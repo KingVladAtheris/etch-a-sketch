a11a28d6746d079756c907c0ddf0a233c5f70b7d
KingVladAtheris <192940540+KingVladAtheris@users.noreply.github.com>
2025-01-05 10:37:34 +0200

    feat: make a grid of divs based on input

---
+++ script.js
@@ -0,0 +1,26 @@
+//Setting up consts
+const buttonInput = document.querySelector("#user-input");
+const buttonPress = document.querySelector("#user-button");
+const divContainer = document.querySelector("#container");
+
+
+//Button logic
+buttonPress.addEventListener("click", () =>{
+    const squaresNumber = parseInt(buttonInput.value, 10);
+    if(isNaN(squaresNumber) || squaresNumber <= 0){
+        alert("Please enter a number higher than 0.");
+    }
+    else{
+        for (let i = 0; i <= squaresNumber; i++){
+            const dynamicDiv = document.createElement("div");
+            const numberOfColumns = Math.floor(Math.sqrt(squaresNumber));
+            dynamicDiv.style.border = "1px solid black";
+            dynamicDiv.style.flex = `0 1 ${100 / numberOfColumns}%`;
+            dynamicDiv.style.height = `${100 / numberOfColumns}%`;
+            dynamicDiv.style.boxSizing = "border-box";
+            divContainer.appendChild(dynamicDiv);
+        }
+    }
+    
+
+});
